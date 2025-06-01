6dbccfeccecb85ed4fdacce0a6983ac5444f7ed8
ryder keeny <keenyryder@gmail.com>
2025-06-01 17:37:01 -0400

    media gallery set up

--- my-website/src/content/about.jsx
+++ my-website/src/content/about.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
 
-import './carousel.css'
+import { Card, CardImg, CardTitle, Col, Container, Row, ProgressBar, CardBody, Tabs, Tab } from 'react-bootstrap'
 import { HydratedRouter } from 'react-router-dom'
+import './about.css'
+
 
 function About() {
 
   return (
     <main className='about'>
- 
-        <p>this is the section including contact and about info</p>
-
-
+      
+    <h1> Ryder Keeny </h1>
+    <hr/>
+    <h2> Interests Hobbies </h2>
+    
+    <h2> Education </h2>
+    
+    <h2> Leadership Roles </h2>
+    
+    <h2> Work History </h2>
+    
+    <h2> Documents </h2>
+      
 
     </main>
   )
--- my-website/src/content/carousel.jsx
+++ my-website/src/content/carousel.jsx
@@ -1,58 +1,187 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, CardImg, CardTitle, Col, Container, Row, ProgressBar, CardBody, Tabs, Tab, CarouselItem } from 'react-bootstrap'
+
 
 function ImageCarousel() {
   return (
-    <main className="carousel">
-      <p>This is the section including Images</p>
+    <Tabs defaultActiveKey={"purrPlay"}>
+      <Tab className="carousel" eventKey='purrPlay' title='PurrPlay'>
+        <Carousel interval={3000}>
 
-      <Carousel interval={3000}>
+          {/* First slide */}
+          <Carousel.Item>
+            <img
+              className="d-block mx-auto"
+              src="/PurrPlay_1.png"
+              alt="Slide 1"
+              style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}
+            />
+          </Carousel.Item>
 
-        {/* First slide */}
-        <Carousel.Item>
-          <img
+          {/* Second slide */}
+          <Carousel.Item>
+            <img
+              className="d-block mx-auto"
+              src="/PurrPlay_2.png"
+              alt="Slide 2"
+              style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}
+            />
+          </Carousel.Item>
+
+          {/* Third slide */}
+          <Carousel.Item>
+            <img
+              className="d-block mx-auto"
+              src="/PurrPlay_3.png"
+              alt="Slide 3"
+              style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Tab>
+
+      <Tab eventKey='Fluencia' title='Fluencia' >
+        <Carousel>
+          <Carousel.Item>
+            <img src='/Fluencia_1.png' 
             className="d-block mx-auto"
-            src="/I1.png"
-            alt="Slide 1"
             style={{
-              height: '500px',
-              width: 'auto',
-              objectFit: 'contain'
-            }}
-          />
-        </Carousel.Item>
-
-        {/* Second slide */}
-        <Carousel.Item>
-          <img
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+          <Carousel.Item>
+          <img src='/Fluencia_2.png' 
             className="d-block mx-auto"
-            src="/I2.png"
-            alt="Slide 2"
             style={{
-              height: '500px',
-              width: 'auto',
-              objectFit: 'contain'
-            }}
-          />
-        </Carousel.Item>
-
-        {/* Third slide */}
-        <Carousel.Item>
-          <img
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+          <Carousel.Item>
+          <img src='/Fluencia_3.png' 
             className="d-block mx-auto"
-            src="/I3.png"
-            alt="Slide 3"
             style={{
-              height: '500px',
-              width: 'auto',
-              objectFit: 'contain'
-            }}
-          />
-        </Carousel.Item>
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+        </Carousel>
+      </Tab>
+      
+      <Tab eventKey='CultureConnect' title='CultureConnect' >
+        <Carousel>
+          <Carousel.Item>
+          <img src='/CultureConnect_1.png' 
+            className="d-block mx-auto"
+            style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+          <Carousel.Item>
+          <img src='/CultureConnect_2.png' 
+            className="d-block mx-auto"
+            style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+          <Carousel.Item>
+          <img src='/CultureConnect_3.png' 
+            className="d-block mx-auto"
+            style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+        </Carousel>
+      </Tab>
+      
+      <Tab eventKey='BallerzOnly' title='BallerzOnly' >
+        <Carousel>
+          <Carousel.Item>
+          <img src='/Ballerz_1.png' 
+            className="d-block mx-auto"
+            style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+          <Carousel.Item>
+          <img src='/Ballerz_2.png'
+            className="d-block mx-auto"
+            style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+          <Carousel.Item>
+          <img src='/Ballerz_3.png' 
+            className="d-block mx-auto"
+            style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+        </Carousel>
+      </Tab>
 
-      </Carousel>
-    </main>
+      <Tab eventKey='Bytes Of Love' title='BOL' >
+        <Carousel>
+          <Carousel.Item>
+          <img src='/BOL_menu.gif' 
+            className="d-block mx-auto"
+            style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+          <Carousel.Item>
+          <img src='/BOL_2.png' 
+            className="d-block mx-auto"
+            style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+          <Carousel.Item>
+          <img src='/BOL_3.png' 
+            className="d-block mx-auto"
+            style={{
+                height: '500px',
+                width: 'auto',
+                objectFit: 'contain'
+              }}/>
+          </Carousel.Item>
+        </Carousel>
+      </Tab>
+    </Tabs>
   );
 }
 
